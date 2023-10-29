a8ed4e73f6b6b8b4577ea69890d18a1e08ff2112
naisu-dev <118091656+naisu-dev@users.noreply.github.com>
2023-10-30 01:41:30 +0900

    Update main.py

--- main.py
+++ main.py
@@ -16,5 +16,31 @@ def main():
       return abort(400,"Argument Error")
 ## https://example.com/fibonacci/?x=1&y=1&count=10
 
+@app.route("/aliquot/",methods=["GET"])
+def main():
+    try:
+      x = int(request.args.get("x"))
+      z = [i for i in range(1, x+1) if x % i ==0]
+      if len(z) == 2:
+        return z
+      else:
+        answer = [x]
+        while True:
+          if len(z) == 2:
+            answer.append(1)
+            return answer
+            break
+          else:
+            del z[len(z)-1]
+            sumz = sum(z)
+            z = [i for i in range(1, sum(z)+1) if sum(z) % i ==0]
+            answer.append(sumz)
+            if answer[len(answer)-1] == answer[len(answer)-2]:
+              return [x]
+              break
+    except:
+      return abort(400,"Argument Error")
+## https://example.com/aliquot/?x=12
+
 if __name__ == "__main__":
   app.run(host="0.0.0.0",port=4000)
