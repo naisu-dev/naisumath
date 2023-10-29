9339ab5937d621eb715b0287423362325e505e94
naisu-dev <118091656+naisu-dev@users.noreply.github.com>
2023-10-30 00:18:10 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, request, abort
 
 app = Flask(__name__)
 
-@app.route("/fibonacci",methods=["GET"])
+@app.route("/fibonacci/",methods=["GET"])
 def main():
     try:
       x = int(request.args.get("x"))
@@ -14,7 +14,7 @@ def main():
       return z
     except:
       return abort(400,"Argument Error")
-## https://example.com/?x=1&y=1&count=10
+## https://example.com/fibonacci?x=1&y=1&count=10
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0",port=4000)
