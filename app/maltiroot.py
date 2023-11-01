f6432e0b9aa9bca018297555d31ea2cc9333c60b
naisu-dev <118091656+naisu-dev@users.noreply.github.com>
2023-11-01 16:44:46 +0900

    Update maltiroot.py

--- app/maltiroot.py
+++ app/maltiroot.py
@@ -1,12 +1,10 @@
 ## https://gist.github.com/naisu-dev/b0a5b5227ee31b8875694ca818132ac7
 ## https://replit.com/@naisu-dev/multiroot?s=app
 
-def malti_root(x:int, y:int):
-  z = x**(1/y)
-  if x == z**y:
-    return round(z)
-  else:
-    return z
+def malti_root(x: int, y: int):
+  z = x**(1 / y)
+  z = x**(1 / y)
+  return round(z)
 
 ## print(malti_root(64, 3))
 
@@ -14,19 +12,17 @@ from flask import Flask, request, abort
 
 app = Flask(__name__)
 
-@app.route("/",methods=["GET"])
+@app.route("/", methods=["GET"])
 def main():
-    try:
-      x = int(request.args.get("x"))
-      y = int(request.args.get("y"))
-      z = x**(1/y)
-      if x == z**y:
-        return round(z)
-      else:
-        return z
-    except:
-      return abort(400,"Argument Error")
+  try:
+    x = int(request.args.get("x"))
+    y = int(request.args.get("y"))
+    z = x**(1 / y)
+    return round(z)
+  except:
+    return abort(400, "Argument Error")
+
 ## https://example.com/?x=27&y=3
 
 if __name__ == "__main__":
-  app.run(host="0.0.0.0",port=4000)
+  app.run(host="0.0.0.0", port=4000)
